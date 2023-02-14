6f457ef1041b761b4a565dfc7a6151e2dde7079f
Ushie <ushiekane@gmail.com>
2023-02-14 22:30:52 +0300

    refactor: eslint

--- src/components/molecules/Hero.svelte
+++ src/components/molecules/Hero.svelte
@@ -5,12 +5,12 @@
 
 	// let avatar_id, avatar = "https://github.com/ushie.png";
 	// onMount (() => {
-    //     LanyardData.subscribe(async (e) => {
-    //         let data = await e;
-    //         avatar_id = data.data.discord_user.avatar;
-    //         avatar = "https://cdn.discordapp.com/avatars/399862294143696897/" + avatar_id + ".png?size=2048";
-    //     });
-    // });
+	//     LanyardData.subscribe(async (e) => {
+	//         let data = await e;
+	//         avatar_id = data.data.discord_user.avatar;
+	//         avatar = "https://cdn.discordapp.com/avatars/399862294143696897/" + avatar_id + ".png?size=2048";
+	//     });
+	// });
 </script>
 
 <div class="logo-container">
@@ -39,7 +39,7 @@
 		.hero-title-mobile {
 			display: inline-block;
 			color: var(--pinky);
-			font-size: clamp(2.5rem, 8vw, 5rem); 
+			font-size: clamp(2.5rem, 8vw, 5rem);
 		}
 	}
 </style>
--- src/stores/LanyardStore.js
+++ src/stores/LanyardStore.js
@@ -1,11 +1,11 @@
-import { readable } from "svelte/store";
+import { readable } from 'svelte/store';
 
 const fetchLanyard = () => {
-    const json = fetch('https://api.lanyard.rest/v1/users/399862294143696897').then((r) => r.json());
-    console.log(json);
-    return json;
+	const json = fetch('https://api.lanyard.rest/v1/users/399862294143696897').then((r) => r.json());
+	console.log(json);
+	return json;
 };
 
 const LanyardData = readable(fetchLanyard());
 console.log(LanyardData);
-export default LanyardData; 
+export default LanyardData;
